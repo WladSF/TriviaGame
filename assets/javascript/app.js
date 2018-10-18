255648c7d91e3561ed7a582ef60801da199a8be6
WladSF <wpaivasf@gmail.com>
2018-10-18 01:35:35 -0700

    Added some js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,86 +6,88 @@
 //and correctAnswer will have the value of a string.
 
 var questions = [{
-    question: "What is the capital of France?",
-    answers: ["Paris", "Luxembourg", "Barcelona", "Belgium"],
-    correctAnswer: "Paris"
+    question: "Which movie was not made in the 80's?",
+    answers: ["Breakfast Club", "Revenge Of The Nerds", "Ferris Buillers Day Off", "Star Wars Episode 4 : A New Hope"],
+    correct: 3
 }, {
-    question: "What is the capital of France?",
-    answers: ["Paris", "Luxembourg", "Barcelona", "Belgium"],
-    correctAnswer: "Paris"
+    question: "The Material Girl:",
+    answers: ["Cindy Lauper", "Bonnie Tyler", "Madonna", "Chaka Khan"],
+    correct: 2
 }, {
-    question: "What is the capital of France?",
-    answers: ["Paris", "Luxembourg", "Barcelona", "Belgium"],
-    correctAnswer: "Paris"
+    question: "One of her earliest hits was 'I wanna dance with somebody:",
+    answers: ["Cher","Whitney Houston", "Britney Spears", "Tiffany"],
+    correct: 1
 }, {
-    question: "What is the capital of France?",
-    answers: ["Paris", "Luxembourg", "Barcelona", "Belgium"],
-    correctAnswer: "Paris"
+    question: "Video game where the main character jumps over crocodiles, lagoons and scorpions in the jungle:",
+    answers: ["Pitfall", "River Raid", "Enduro", "Atari"],
+    correct: 0
 }, {
-    question: "What is the capital of France?",
-    answers: ["Paris", "Luxembourg", "Barcelona", "Belgium"],
-    correctAnswer: "Paris"
+    question: "Which dance was made famous by a scene in 'Can't Buy me Love' (1987):",
+    answers: ["The Wave", "Break Dancing", "Tektonic", "African Anteater Ritual"],
+    correct: 3
 }, {
-    question: "What is the capital of France?",
-    answers: ["Paris", "Luxembourg", "Barcelona", "Belgium"],
-    correctAnswer: "Paris"
+    question: "What dog had the craziest laugh?",
+    answers: ["Barfy", "Muttley", "Dug the Dog", "McBarker (Mr. Magoo)"],
+    correct: 1
 }, {
-    question: "What is the capital of France?",
-    answers: ["Paris", "Luxembourg", "Barcelona", "Belgium"],
-    correctAnswer: "Paris"
+    question: "What is the second best-selling album of all times ?",
+    answers: ["Pink Floyd the Wall - 1982", "Prince purple Rain - 1984", "Madoona Like a Virgin - 1984", "Michael Jackson Thriller - 1983"],
+    correct: 3
 }, {
-    question: "What is the capital of France?",
-    answers: ["Paris", "Luxembourg", "Barcelona", "Belgium"],
-    correctAnswer: "Paris"
+    question: "MTV launched in August of....",
+    answers: ["1980", "1981", "1983", "1985"],
+    correct: 2
 }, {
-    question: "What is the capital of France?",
-    answers: ["Paris", "Luxembourg", "Barcelona", "Belgium"],
-    correctAnswer: "Paris"
+    question: "What are two fashion statements from the 80's?",
+    answers: ["Crazy hair and shoulder pads", "Bell-bottom pants and hairspray", "High heels and snickers", "Colorful jewelry and sofisticated shoes"],
+    correct: 0
 }, {
-    question: "What is the capital of France?",
-    answers: ["Paris", "Luxembourg", "Barcelona", "Belgium"],
-    correctAnswer: "Paris"
+    question: "What is the name of the good Gremlin?",
+    answers: ["Goodie", "That cool gremlin", "Gizmo", "Mogwai"],
+    correct: 2
 }];
-
 
 //Part ONE B
 
 //Create a 'timer' variable that will hold setInterval
 var timer;
 
-var questionTracker = 0;
+var userPick = null;
 
-//Create the game object that will have 3 properties and 4 methods
+var currentQuestion = 0;
+
+//Create the game object that will have 4 properties and 4 methods
 var game = {
     correct: 0,
     incorrect: 0,
-    counter: 120,
+    unanswered: 0,
+    counter: 30,
     
 
     countDown: function(){
         game.counter--;
         $("#counter-number").html(game.counter);
         
-        if (game.counter === 0){
+        if (game.counter <= 0){
             console.log("Your time is up!");
             game.done();
         }
     },
 
     done: function(){
+        clearInterval(counter);
 
     },
     
     //After pressing the start button, the timer shows up, 
-    //and first questions and answers, and the 'start' button must disappear//
+    //first questions and answers, and the 'start' button must disappear//
     start: function(){
         var timerDiv = $("<div>").text(game.counter);
-        var questionDiv = $("<div>").text(questions[questionTracker].question);
+        var questionDiv = $("<div>").text(questions[currentQuestion].question);
         $("#quiz-area").append(timerDiv);
         $("#quiz-area").append(questionDiv);
-        $("#quiz-area").append(questions[questionTracker].answers);
-        
-        questionTracker++;
+        $("#quiz-area").append(questions[currentQuestion].answers);       
+        currentQuestion++;
         console.log("gamestarted");
     },
 
@@ -95,8 +97,12 @@ var game = {
 }
  $("#start").on("click", function (){
      game.start();
+     game.countDown();
+     $(this).hide();
+     counter = setInterval(timer, 2000);
  })
 //Next methods to be crteated are done, start, result
-//Cfreate document.clcik events at the end dor each questions when the game starts
+//Create document.clcik events at the end for each questions when the game starts
+
 
 
